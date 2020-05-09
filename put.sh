a4ad1d4498fb6de66f136491cc6d7b9c09607462
npchitman <npchitman@github.com>
2020-05-10 00:47:06 +0800

    完善

--- put.sh
+++ put.sh
@@ -3,4 +3,8 @@
 projpath=$(cd `dirname $0`; pwd)
 keypath=$HOME/.ssh/npchitman.pem
 
+# 生成网页文件
+npm run build
+
+# 部署到服务器
 scp -i $keypath -r $projpath/dist/* root@47.110.143.150:/usr/local/nginx/html/.
